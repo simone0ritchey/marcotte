8bba6099185c79ebbdc8fd0ced1a35837fbf0f8b
simoneritchey <simone.ritchey@gmail.com>
2021-08-25 11:34:15 -0500

    trying to figure out the best way to use python

--- T7_xlink/clean_for_xiNET.R
+++ T7_xlink/clean_for_xiNET.R
@@ -4,11 +4,23 @@
 ## Do First ####################################################################
 
 # Load Libraries
-library(reticulate)
+  library(reticulate)
 
 # Set working directory
-setwd("/Users/simone/Documents/UT/marcotte/T7_xlink")
+  setwd("/Users/simone/Documents/UT/marcotte/T7_xlink")
 
 # Load data
-xlink <- read.csv("2021_08_17_T7_DSSO_Crosslinks.csv")
+  xlink <- read.csv("2021_08_17_T7_DSSO_Crosslinks.csv")
 
+################################################################################
+
+# Removing unwanted columns
+  xlink <- xlink[,c(3,10,11,14,15)]
+
+# Changing column names
+  colnames(xlink) <- c("Score","Protein1","LinkPos1","Protein2","LinkPos2")
+
+# Giving xlink fasta names
+  
+  
+  

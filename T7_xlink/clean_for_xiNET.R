0916855be9a072f46580ed472c97ce25607eb959
simoneritchey <simone.ritchey@gmail.com>
2021-08-25 18:52:29 -0500

    yay we made the dataframe!

--- T7_xlink/clean_for_xiNET.R
+++ T7_xlink/clean_for_xiNET.R
@@ -6,6 +6,7 @@
 # Load libraries
   library(seqinr)
   library(dplyr)
+  library(stringr)
 
 # Set working directory
   setwd("/Users/simone/Documents/UT/marcotte/T7_xlink")
@@ -13,8 +14,43 @@
 # Load data
   xlink <- read.csv("2021_08_17_T7_DSSO_Crosslinks.csv")
   fasta <- read.fasta("T7_phage_UP000000840_10760.fasta") %>% unlist()
-    # Making fasta data frame
-      fasta_dataframe <- data.frame(matrix(fasta, nrow = length(fasta), byrow = TRUE))
+  
+# Making seq_length_tbl function from https://rdrr.io/github/seankross/warppipe/src/R/seq_length_tbl.R
+  seq_length_tbl <- function(path, description = "^>", comment = "^;"){
+        # Read fasta file
+        fasta_file <- readLines(path)
+        
+        # Trim whitespace
+        fasta_file <- str_trim(fasta_file)
+        
+        # Remove comment lines
+        if(any(grepl(comment, fasta_file))){
+          fasta_file <- fasta_file[-grep(comment, fasta_file)]
+        }
+        
+        # Find lines with descriptions
+        description_lines <- grep(description, fasta_file)
+        
+        sections <- list()
+        
+        # Set last description "fencepost" as the last line plus 1
+        fencepost_lines <- c(description_lines, length(fasta_file) + 1)
+        
+        for(i in 1:(length(fencepost_lines) - 1)){
+          sections[[i]] <- seq(fencepost_lines[i] + 1, fencepost_lines[i + 1] - 1)
+        }
+        
+        len <- lapply(sections, 
+                      function(x, file = fasta_file){
+                        sum(str_length(file[x]))
+                      })
+        len <- unlist(len)
+        
+        # Create data frame
+        data.frame(Description = sub(description, "", fasta_file[description_lines]),
+                   sequence_length = len)
+      }
+      
       
 ################################################################################
 
@@ -28,6 +64,6 @@
   xlink$Name <- sub("_","",watershed$SHORT_NAME)
   watershed2012 <- select(watershed, -contains("2011"))
     # I think using the contains is the key
-  
+  fasta_dataframe <- seq_length_tbl("/Users/simone/Documents/UT/marcotte/T7_xlink/T7_phage_UP000000840_10760.fasta")
   
   

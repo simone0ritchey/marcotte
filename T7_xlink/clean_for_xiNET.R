1fd67e24456c722a73d51fd7feda14a8c781e97e
simoneritchey <simone.ritchey@gmail.com>
2021-08-25 18:55:38 -0500

    did a little clean up

--- T7_xlink/clean_for_xiNET.R
+++ T7_xlink/clean_for_xiNET.R
@@ -4,54 +4,51 @@
 ## Do First ####################################################################
 
 # Load libraries
-  library(seqinr)
-  library(dplyr)
   library(stringr)
+
+# Making seq_length_tbl function from https://rdrr.io/github/seankross/warppipe/src/R/seq_length_tbl.R
+  seq_length_tbl <- function(path, description = "^>", comment = "^;"){
+  # Read fasta file
+  fasta_file <- readLines(path)
+  
+  # Trim whitespace
+  fasta_file <- str_trim(fasta_file)
+  
+  # Remove comment lines
+  if(any(grepl(comment, fasta_file))){
+    fasta_file <- fasta_file[-grep(comment, fasta_file)]
+  }
+  
+  # Find lines with descriptions
+  description_lines <- grep(description, fasta_file)
+  
+  sections <- list()
+  
+  # Set last description "fencepost" as the last line plus 1
+  fencepost_lines <- c(description_lines, length(fasta_file) + 1)
+  
+  for(i in 1:(length(fencepost_lines) - 1)){
+    sections[[i]] <- seq(fencepost_lines[i] + 1, fencepost_lines[i + 1] - 1)
+  }
+  
+  len <- lapply(sections, 
+                function(x, file = fasta_file){
+                  sum(str_length(file[x]))
+                })
+  len <- unlist(len)
+  
+  # Create data frame
+  data.frame(Description = sub(description, "", fasta_file[description_lines]),
+             sequence_length = len)
+}
 
 # Set working directory
   setwd("/Users/simone/Documents/UT/marcotte/T7_xlink")
 
 # Load data
   xlink <- read.csv("2021_08_17_T7_DSSO_Crosslinks.csv")
-  fasta <- read.fasta("T7_phage_UP000000840_10760.fasta") %>% unlist()
-  
-# Making seq_length_tbl function from https://rdrr.io/github/seankross/warppipe/src/R/seq_length_tbl.R
-  seq_length_tbl <- function(path, description = "^>", comment = "^;"){
-        # Read fasta file
-        fasta_file <- readLines(path)
-        
-        # Trim whitespace
-        fasta_file <- str_trim(fasta_file)
-        
-        # Remove comment lines
-        if(any(grepl(comment, fasta_file))){
-          fasta_file <- fasta_file[-grep(comment, fasta_file)]
-        }
-        
-        # Find lines with descriptions
-        description_lines <- grep(description, fasta_file)
-        
-        sections <- list()
-        
-        # Set last description "fencepost" as the last line plus 1
-        fencepost_lines <- c(description_lines, length(fasta_file) + 1)
-        
-        for(i in 1:(length(fencepost_lines) - 1)){
-          sections[[i]] <- seq(fencepost_lines[i] + 1, fencepost_lines[i + 1] - 1)
-        }
-        
-        len <- lapply(sections, 
-                      function(x, file = fasta_file){
-                        sum(str_length(file[x]))
-                      })
-        len <- unlist(len)
-        
-        # Create data frame
-        data.frame(Description = sub(description, "", fasta_file[description_lines]),
-                   sequence_length = len)
-      }
-      
-      
+  fasta <- seq_length_tbl("/Users/simone/Documents/UT/marcotte/T7_xlink/T7_phage_UP000000840_10760.fasta")
+
 ################################################################################
 
 # Removing unwanted columns

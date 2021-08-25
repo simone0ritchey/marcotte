175a23d0e555db9ef2a041abd0ff9a94f8f340ca
simoneritchey <simone.ritchey@gmail.com>
2021-08-25 10:25:04 -0500

    worked out kinks

---
+++ T7_xlink/clean_for_xiNET.R
@@ -0,0 +1,10 @@
+# Clean data for xiNET
+# Simone Ritchey for Marcotte lab
+
+## Do First ####################################################################
+
+# Set working directory
+setwd("/Users/simone/Documents/UT/marcotte/T7_xlink")
+
+# Load data
+xlink <- read.csv("2021_08_17_T7_DSSO_Crosslinks.csv")

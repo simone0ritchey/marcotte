30bf61ccb529e844633804675e2d50b4a67c8842
simoneritchey <simone.ritchey@gmail.com>
2021-10-25 15:15:50 -0500

    started working on wheat germ analysis. Need to figure out which function I want to use to select by a partial string match. Also some left over stuff to push from the last analysis

--- Wheat Germ/WG_Fraction_Abundance_Comparison.R
+++ Wheat Germ/WG_Fraction_Abundance_Comparison.R
@@ -1,4 +1,22 @@
 # Wheat Germ MCS Protein Abundance Comparison Between Fractions
-# Simone for Marcotte Lab
 
+### Do First ###################################################################
+
+# Set Working Directory
+  setwd("/Users/simone/Documents/UT/marcotte/Wheat Germ")
+
+# Load Data
+  zerotoforty <- read.csv("2021_09_27_WG_0_40_Proteins.csv")
+  fortytoseventy <- read.csv("2021_09_27_WG_40_70_Proteins.csv")
+
+# Load Packages
+  library(dplyr)
+
+################################################################################
+
+# Test to see if this is the function I want to use
+  zerotoforty_test <- zerotoforty$Description %>% 
+    select(contains(1))
+
+# What are these things related to tRNA ligase
 
